86d70be309c93326b6c20a2e7cf7282f1255ec47
Korolov-Artem <justanotheremaildx@gmail.com>
2026-02-03 16:58:40 +0100

    refactor main.go

--- src/main.go
+++ src/main.go
@@ -35,7 +35,6 @@ func main() {
 
 	book := rc.Rootfiles[0]
 	fmt.Println(book.Title)
-	fmt.Println("--------------------------------------------------")
 
 	spine := book.Spine.Itemrefs
 	currentIndex := 0
@@ -45,128 +44,44 @@ func main() {
 	for {
 		fmt.Print("\033[H\033[2J")
 
-		totalWidth, totalHeight, err := term.GetSize(int(os.Stdout.Fd()))
-		if err != nil || totalWidth <= 0 {
-			totalWidth = 80
-			totalHeight = 24
-		}
-
-		verticalMargin := 5
-		linesPerPage := totalHeight - verticalMargin
-		if linesPerPage < 1 {
-			linesPerPage = 1
-		}
-
 		item := spine[currentIndex]
-
-		docStyle := lipgloss.NewStyle().
-			Width(totalWidth).
-			Align(lipgloss.Center)
-
-		textStyle := lipgloss.NewStyle().
-			Width(min(80, totalWidth-10)).
-			Align(lipgloss.Left)
 
 		fmt.Printf("Chapter %d of %d (ID: %s)\n", currentIndex+1, len(spine), item.ID)
 
+		docStyle, textStyle, totalHeight, totalWidth := createReaderStyles()
+
 		fileName := findFileName(book, item.ID)
+
 		if fileName == "" {
-			fmt.Println("Could not find file for this chapter.")
-			fmt.Println("\nPress 'n' for next, 'p' for prev.")
+			handleEmptyText(docStyle, reader, &currentIndex, spine)
+			continue
+		}
+
+		fileStream, err := openZipFile(zip, fileName)
+		if err != nil {
 			handleNavigation(reader, &currentIndex, len(spine))
 			continue
+		}
+
+		rawText := extractText(fileStream)
+		fileStream.Close()
+		formattedText := strings.ReplaceAll(rawText, "\t", "    ")
+
+		if strings.TrimSpace(rawText) == "" {
+			handleEmptyText(docStyle, reader, &currentIndex, spine)
+			continue
 		} else {
-			fileStream, err := openZipFile(zip, fileName)
-			if err != nil {
-				handleNavigation(reader, &currentIndex, len(spine))
-				continue
-			}
-			rawText := extractText(fileStream)
-			formattedText := strings.ReplaceAll(rawText, "\t", "    ")
-			fileStream.Close()
+			viewChapter(totalHeight, totalWidth, textStyle, docStyle, formattedText, reader, &currentIndex, spine)
+		}
 
-			if strings.TrimSpace(rawText) == "" {
-				fmt.Println("\n[This chapter contains no text (Image or Empty)]")
-				fmt.Println(docStyle.Render("\n[ Press (n) Next or (p) Prev ]"))
-				handleNavigation(reader, &currentIndex, len(spine))
-				continue
-			} else {
-				fullFormattedText := textStyle.Render(formattedText)
-				allLines := strings.Split(fullFormattedText, "\n")
-
-				totalLines := len(allLines)
-				totalPages := (totalLines + linesPerPage - 1) / linesPerPage
-
-				currentPage := 0
-
-				for {
-
-					startLine := currentPage * linesPerPage
-					endLine := startLine + linesPerPage
-					if endLine > totalLines {
-						endLine = totalLines
-					}
-
-					pageLines := allLines[startLine:endLine]
-
-					pageContent := strings.Join(pageLines, "\n")
-					fmt.Println(docStyle.Render(pageContent))
-
-					linesPrinted := endLine - startLine
-					if linesPrinted < linesPerPage {
-						fmt.Print(strings.Repeat("\n", linesPerPage-linesPrinted))
-					}
-
-					status := fmt.Sprintf("\nPage %d/%d | Chapter %d/%d", currentPage+1, totalPages, currentIndex+1, len(spine))
-					fmt.Println(docStyle.Render(status))
-
-					footerText := "[ (n) Next Pg | (p) Prev Pg | (N) Next Chap | (P) Prev Chap | (q) Quit ]"
-					fmt.Println(docStyle.Render(footerText))
-
-					input, _ := reader.ReadString('\n')
-					input = strings.TrimSpace(input)
-
-					shouldBreak := false
-
-					switch input {
-					case "n":
-						if currentPage < totalPages-1 {
-							currentPage++
-						} else {
-							currentIndex++
-							shouldBreak = true
-						}
-					case "p":
-						if currentPage > 0 {
-							currentPage--
-						} else {
-							currentIndex--
-							shouldBreak = true
-						}
-					case "N":
-						currentIndex++
-						shouldBreak = true
-					case "P":
-						currentIndex--
-						shouldBreak = true
-					case "q":
-						return
-					default:
-						fmt.Println("Invalid input")
-					}
-					if shouldBreak {
-						break
-					}
-				}
-			}
-			if currentIndex >= len(spine) {
-				currentIndex = len(spine) - 1
-			}
-			if currentIndex < 0 {
-				currentIndex = 0
-			}
+		if currentIndex >= len(spine) {
+			currentIndex = len(spine) - 1
+		}
+		if currentIndex < 0 {
+			currentIndex = 0
 		}
 	}
+
 }
 
 func findFileName(book *epub.Rootfile, chapterId string) string {
@@ -218,9 +133,9 @@ func handleNavigation(reader *bufio.Reader, currentIndex *int, totalChapters int
 	input = strings.TrimSpace(input)
 
 	switch input {
-	case "n", "N":
+	case "n":
 		*currentIndex++
-	case "p", "P":
+	case "p":
 		*currentIndex--
 	case "q":
 		os.Exit(0)
@@ -231,5 +146,107 @@ func handleNavigation(reader *bufio.Reader, currentIndex *int, totalChapters int
 	}
 	if *currentIndex < 0 {
 		*currentIndex = 0
+	}
+}
+
+func handleEmptyText(docStyle lipgloss.Style, reader *bufio.Reader, currentIndex *int, spine []epub.Itemref) {
+	fmt.Println("\n[This chapter contains no text (Image or Empty)]")
+	fmt.Println(docStyle.Render("\n[ Press (n) Next or (p) Prev ]"))
+	handleNavigation(reader, currentIndex, len(spine))
+}
+
+func createReaderStyles() (docStyle, textStyle lipgloss.Style, totalHeight, totalWidth int) {
+	totalWidth, totalHeight, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil || totalWidth <= 0 {
+		totalWidth = 80
+		totalHeight = 64
+	}
+
+	docStyle = lipgloss.NewStyle().
+		Width(totalWidth).
+		Align(lipgloss.Center)
+
+	textStyle = lipgloss.NewStyle().
+		Width(min(80, totalWidth-10)).
+		Align(lipgloss.Left)
+
+	return docStyle, textStyle, totalHeight, totalWidth
+}
+
+func viewChapter(totalHeight, totalWidth int, textStyle, docStyle lipgloss.Style, formattedText string, reader *bufio.Reader, currentIndex *int, spine []epub.Itemref) {
+	verticalMargin := 7
+	linesPerPage := totalHeight - verticalMargin
+	if linesPerPage < 1 {
+		linesPerPage = 1
+	}
+
+	textWidth := 80
+	if totalWidth-10 < 80 {
+		textWidth = totalWidth - 10
+	}
+	leftPadding := (totalWidth - textWidth) / 2
+	if leftPadding < 0 {
+		leftPadding = 0
+	}
+
+	pageStyle := lipgloss.NewStyle().Padding(1, 0, 1, leftPadding)
+
+	fullFormattedText := textStyle.Render(formattedText)
+	allLines := strings.Split(fullFormattedText, "\n")
+
+	totalLines := len(allLines)
+	totalPages := (totalLines + linesPerPage - 1) / linesPerPage
+
+	currentPage := 0
+
+	for {
+		startLine := currentPage * linesPerPage
+		endLine := startLine + linesPerPage
+		if endLine > totalLines {
+			endLine = totalLines
+		}
+
+		pageLines := allLines[startLine:endLine]
+
+		pageContent := strings.Join(pageLines, "\n")
+		pageContent = strings.TrimPrefix(pageContent, "\n")
+
+		fmt.Print("\033[H\033[2J")
+
+		fmt.Println(pageStyle.Render(pageContent))
+
+		linesPrinted := endLine - startLine
+		if linesPrinted < linesPerPage {
+			fmt.Print(strings.Repeat("\n", linesPerPage-linesPrinted))
+		}
+
+		status := fmt.Sprintf("\nPage %d/%d | Chapter %d/%d", currentPage+1, totalPages, *currentIndex+1, len(spine))
+		fmt.Println(docStyle.Render(status))
+		//footerText := "[ (n) Next Pg | (p) Prev Pg | (q) Quit ]"
+		//fmt.Println(docStyle.Render(footerText))
+
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "n":
+			if currentPage < totalPages-1 {
+				currentPage++
+			} else {
+				*currentIndex++
+				return
+			}
+		case "p":
+			if currentPage > 0 {
+				currentPage--
+			} else {
+				*currentIndex--
+				return
+			}
+		case "q":
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid input")
+		}
 	}
 }
